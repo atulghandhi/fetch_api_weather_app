8b8b48cf4d276693f6c0978cc165342186c3b461
bobyghandhi <bobyghandhi@gmail.com>
2020-12-21 22:06:30 +0000

    added server code

---
+++ server.js
@@ -0,0 +1,35 @@
+//Empty object to act as the endpoint to all our
+const projectData = {}
+
+//express server, install and use
+const express = require('express');
+const app = express();
+
+//body parser middleware to translate between json/sting for server/local communication
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//cors for browser server communication without security interruptions
+const cors = require('cors');
+app.use(cors());
+
+//main project root folder
+app.use(express.static('website'));
+
+//set up server with listen method
+const port = 3000;
+const server = app.listen(port, () => console.log('running on fucking port ' + port));
+
+app.get('/all', (req, res) => {
+    res.send(projectData);
+})
+
+let counter = 0;
+
+//set up a POST route. When a POST request is made to this URL, this code is run
+app.post('/addData', (req, res) => {
+    console.log(req.body); //log added 'data' to console
+    projectData[`data_${counter}`] = req.body; //add data to projectData - app endpoint
+    counter++;
+});
---
+++ website/app.js
@@ -0,0 +1,3 @@
+const api_key = "72d5419403015e5d91da40f389f34892";
+let city_name = 'London'
+const url = `pro.openweathermap.org/data/2.5/forecast/hourly?q=${city_name}&appid=${api_key}`;
